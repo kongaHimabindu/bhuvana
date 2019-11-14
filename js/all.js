a47a3f4c2b1f01a7514217e3af0b36b4bae1e4b2
konga Himabindu <you@example.com>
2019-11-14 10:40:07 +0530

    changes

---
+++ js/all.js
@@ -0,0 +1,24 @@
+// function name(){
+//  let a=10;
+//  console.log(a);
+// }
+// // console.log(a);
+// name();
+var x = 10;
+// Here x is 10
+
+{
+    console.log(x);
+  var x = 2;
+  // Here x is 2
+  console.log(x);
+}
+console.log(x);
+// let a=10;
+// console.log(a);
+// {
+//     // console.log(a);
+//     let a=2;
+//     console.log(a);
+// }
+// console.log(a);
